8fdc8207384766cf59ef0d6fbedb3fff56d9d765
nadjib <ndjbchch@gmail.com>
2025-12-24 12:02:48 +0100

    add welcome page as main activity, link it with login and sign up pages

--- app/src/main/java/com/example/epermis/MainActivity.java
+++ app/src/main/java/com/example/epermis/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.epermis;
 
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.content.Intent;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button btnSignUp = findViewById(R.id.btnSignUp);
+        TextView txtCopyright = findViewById(R.id.txtCopyright);
+//        onclick listner usage:
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+//            explicit intent
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Signup.class);
+                startActivity(intent);
+            }
+        });
+        txtCopyright.setOnClickListener(new View.OnClickListener() {
+            @Override
+//            implicit intent
+            public void onClick(View v) {
+                String url = "https://nadjib-chacha.vercel.app/";
+                android.content.Intent intent = new android.content.Intent(android.content.Intent.ACTION_VIEW);
+                intent.setData(android.net.Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+    }
+    public void go_login(View view) {
+        // Intent to open Login Activity
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
